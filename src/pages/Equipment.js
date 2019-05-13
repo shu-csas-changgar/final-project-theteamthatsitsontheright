fb92be0552222ca5e0bd79b4b7f59227a49f3734
Davis Cook <daviscook@outlook.com>
2019-05-13 01:28:46 -0400

    Update Equip and Res

--- src/pages/Equipment.js
+++ src/pages/Equipment.js
@@ -12,11 +12,12 @@ class Equipment extends Component {
             viewModalShow: false,
             createModalShow: false,
             equipment_focus: null,
+            offices: [],
             search: '',
             search_field: 'equipment_name',
             equipment_type: '1',
             contract_type: '1',
-            error: ''
+            error: '',
         };
 
         this.openViewModal = (item) => {
@@ -44,6 +45,7 @@ class Equipment extends Component {
         };
 
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount = () => {
@@ -89,9 +91,10 @@ class Equipment extends Component {
                 this.setState({ error: 'Bad response from the server' })
             }
             return response.json();
-        }).then((data) => {
+       }).then((data) => {
             this.setState({ equipment: data });
-            console.log(this.state.equipment);
+            this.setState({ offices: data});
+            console.log(data);
         }).catch((error) => {
             console.log(error);
             this.setState({ error: error });
@@ -122,6 +125,41 @@ class Equipment extends Component {
             this.setState({'error': error});
         });
     };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const data = {
+            new_equipment_name: this.state.new_equipment_name,
+            new_equipment_type: this.state.new_equipment_type,
+            new_contract_type: this.state.new_contract_type,
+            new_vendor_name: this.state.new_vendor_name,
+            new_office_name: this.state.new_office_name,
+        };
+        fetch('/equipment/new', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            console.log(data);
+            if (response.status >= 400) {
+                this.setState({ error: 'Bad response from the server.' })
+            }
+            return response.json();
+        }).then((data) => {
+            if (data === 'That equipment already exists.') {
+                this.setState({ error: data })
+            } else {
+                this.props.userHasAuthenticated(true);
+                this.props.loginUser(data[0]);
+                this.props.history.push('/')
+            }
+        }).catch((error) => {
+                console.log(error);
+                this.setState({ error: error });
+            }
+        );
+    };
+
 
     render() {
         return (
@@ -182,7 +220,7 @@ class Equipment extends Component {
                                             })
                                         }
                                     </FormControl>
-                                </FormGroup>
+                               </FormGroup>
                             </Col>
                             <Col>
                                 <Button
@@ -227,11 +265,56 @@ class Equipment extends Component {
                                             View/Edit
                                         </Button>
                                     </td>
-                                </tr>
+                               </tr>
                             )
                         }
                         </tbody>
                     </table>
+                    <Modal
+                        show={this.state.show}
+                        onHide={this.handleHide}
+                        aria-labelledby="contained-modal-title-vcenter"
+                        size={'lg'}
+                        centered
+                    >
+                        <Modal.Header closeButton={true}>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Equipment View
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {
+                                (this.state.equipment == null) ?
+                                    <div>null</div> :
+                                    <div>
+                                        <Row>
+                                            <Col>
+                                                <h5>Equipment Name: {this.state.equipment_name}</h5>
+                                            </Col>
+                                            <Col>
+                                                <h5>Equipment ID: {this.state.equipment_id}</h5>
+                                            </Col>
+                                            <Col>
+                                                <h5>Equipment Type: {this.state.type_name}</h5>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <h5>Contract Type: {this.state.contract_type}</h5>
+                                            </Col>
+                                            <Col>
+                                                <h5>Vendor Name: {this.state.vendor_name}</h5>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <h5>Office: {this.state.office_name}</h5>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                            }
+                        </Modal.Body>
+                    </Modal>
                     <Modal
                         show={this.state.createModalShow}
                         onHide={this.hideCreateModal}
@@ -245,8 +328,77 @@ class Equipment extends Component {
                             </Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-
+                            <form noValidate onSubmit={this.handleSubmit}>
+                                <h3>{this.state.error}</h3>
+                                <FormGroup controlId={'equipment_name'} bssize={'large'}>
+                                    <h3>Equipment Name</h3>
+                                    <FormControl
+                                        type={'string'}
+                                        value={this.state.equipment_name}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                                <FormGroup controlId={'equipment_type'} bssize={'large'}>
+                                    <h3>Equipment Type</h3>
+                                    <FormControl
+                                        type={'string'}
+                                        value={this.state.equipment_type}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                                <FormGroup controlId={'contract_type'} bssize={'large'}>
+                                    <h3>Ownership Type</h3>
+                                    <FormControl as={'select'} onChange={this.handleChange}>
+                                        {
+                                            this.state.contract_types.map((type) => {
+                                                return (
+                                                    <option
+                                                        value={type.contract_type_id}
+                                                        key={type.contract_type_id}
+                                                    >
+                                                        {type.contract_name}
+                                                    </option>
+                                                )
+                                            })
+                                        }
+                                    </FormControl>
+                               </FormGroup>
+                                <FormGroup controlId={'vendor_name'} bssize={'large'}>
+                                    <h3>Vendor Name</h3>
+                                    <FormControl
+                                        value={this.state.vendor_name}
+                                        onChange={this.handleChange}
+                                        type={"string"}
+                                    />
+                                </FormGroup>
+                                <FormGroup controlId={'office_id'} bssize={'large'}>
+                                    <h3>Select Your Office</h3>
+                                    <FormControl as={'select'} onChange={this.handleChange}>
+                                        <option value={0}>Make a selection</option>
+                                        {
+                                            this.state.offices.map(office => {
+                                                return(
+                                                    <option key={office.office_id} value={office.office_id}>{office.office_name}</option>
+                                                )
+                                            })
+                                        }
+                                    </FormControl>
+                                </FormGroup>
+                            </form>
                         </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary"
+                                    onClick={this.hideCreateModal}>
+                                Close
+                            </Button>
+                            <Button variant="primary"
+                            block
+                            bssize={"large"}
+                            type={"submit"}
+                            >
+                                Add Item
+                            </Button>
+                        </Modal.Footer>
                     </Modal>
                 </div>
             </div>
--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,10 +1,198 @@
 import React, { Component } from "react";
+import { Form, Row, Col, FormControl, FormGroup, Button, Modal } from "react-bootstrap";
 
 class Reservations extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reservation: [],
+            reservation_types: [],
+            contract_types: [],
+            viewModalShow: false,
+            createModalShow: false,
+            reservation_focus: null,
+            search: '',
+            search_field: 'reservation_name',
+            reservation_type: '1',
+            contract_type: '1',
+            error: ''
+        };
+
+        this.openViewModal = (item) => {
+            this.setState({ viewModalShow: true });
+            this.setState({ reservation_f: item });
+        };
+
+        this.hideViewModal = () => {
+            this.setState({ viewModalShow: false });
+            this.setState({ reservation_f: null });
+        };
+
+        this.openCreateModal = () => {
+            this.setState({ createModalShow: true });
+        };
+
+        this.hideCreateModal = () => {
+            this.setState({ createModalShow: false });
+        };
+
+        this.handleChange = (event) => {
+            this.setState({
+                [event.target.id]: event.target.value
+            });
+        };
+
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    componentDidMount = () => {
+        // This is used to get the contract_types for the filter dropdown.
+        if (this.state.reservation_types.length === 0) {
+            fetch('/reservation/contract_types', {
+                method: 'GET'
+            }).then((response) => {
+                if (response.status >= 400) {
+                    this.setState({error: 'Bad response from the server'})
+                }
+                return response.json();
+            }).then((data) => {
+                this.setState({contract_types: data});
+            }).catch((error) => {
+                console.log(error);
+                this.setState({error: error});
+            });
+        }
+
+        // This is used to get a list of the reservations for the table.
+        fetch('/reservation', {
+            method: 'GET'
+        }).then((response) => {
+            if (response.status >= 400) {
+                this.setState({ error: 'Bad response from the server' })
+            }
+            return response.json();
+        }).then((data) => {
+            this.setState({ reservation: data });
+            console.log(this.state.reservation);
+        }).catch((error) => {
+            console.log(error);
+            this.setState({ error: error });
+        })
+    };
+
+    handleSearch = (event) => {
+        event.preventDefault();
+        const data = {
+            search: this.state.search,
+            search_field: this.state.search_field,
+            contract_type: this.state.contract_type,
+            reservation_type: this.state.reservation_type
+        };
+        fetch('/reservation/filter', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if (response.status >= 400) {
+                this.setState({error: 'Bad response from the server'});
+            }
+            return response.json();
+        }).then((data) => {
+            this.setState({ reservation: data});
+        }).catch((error) => {
+            console.log(error);
+            this.setState({'error': error});
+        });
+    };
+
     render() {
         return (
             <div>
-                <h1>Reservations</h1>
+                <h1>reservation</h1>
+                <div className={'container'}>
+                    <Form noValidate onSubmit={this.handleSearch}>
+                        <Row>
+                            <Col>
+                                <FormGroup controlId={'search'} bssize={'large'}>
+                                    <FormControl
+                                        type={'string'}
+                                        value={this.state.search}
+                                        onChange={this.handleChange}
+                                        />
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                <FormGroup controlId={'search_field'} bssize={'large'}>
+                                    <FormControl as={'select'} onChange={this.handleChange}>
+                                        <option key={'reservation_name'} value={'reservation_name'}>Reservation Name</option>
+                                        <option key={'room_id'} value={'room_id'}>Room ID</option>
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                <Button
+                                    block
+                                    bssize={"small"}
+                                    type={"submit"}
+                                >
+                                    Filter
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                    <Button
+                        block
+                        bssize={'large'}
+                        onClick={this.openCreateModal}
+                        >
+                        Add new Reservation
+                    </Button>
+                    <table className={"table table-hover"}>
+                        <thead>
+                        <tr>
+                            <th>Reservation Name</th>
+                            <th>Room ID</th>
+                            <th>Start Time</th>
+                            <th>End Time</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.reservation.map(item =>
+                                <tr key={item.reservation_id}>
+                                    <td>{item.reservation_name}</td>
+                                    <td>{item.type_name}</td>
+                                    <td>{item.contract_name}</td>
+                                    <td>{item.vendor_name}</td>
+                                    <td>{item.office_name}</td>
+                                    <td>
+                                        <Button variant="primary" onClick={() => this.openViewModal(item)}>
+                                            View/Edit
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                        </tbody>
+                    </table>
+                    <Modal
+                        show={this.state.createModalShow}
+                        onHide={this.hideCreateModal}
+                        aria-labelledby="contained-modal-title-vcenter"
+                        size={'lg'}
+                        centered
+                    >
+                        <Modal.Header closeButton={true}>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Add new Reservation
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+
+                        </Modal.Body>
+                    </Modal>
+                </div>
             </div>
         )
     }
